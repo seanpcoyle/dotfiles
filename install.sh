aef7b4a49e3ef9f888ce93d4643b230f5144955f
Sean Coyle <seanpcoyle@gmail.com>
2023-08-16 11:13:46 -0500

    Symlink before running "brew bundle"

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ create_links() {
   local dir="${1:?dir is missing}"
 
   if [[ -d "${dir}" ]]; then
-    cp -fvas "${dir}/." "${HOME}"
+    cp -rsvf "${dir}/." "${HOME}" 2>/dev/null || :
   fi
 }
 
@@ -38,15 +38,6 @@ if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-brew bundle --file="${HOME}/.Brewfile"
-
-mkdir -p "${HOME}/.oh-my-zsh/themes/powerlevel10k"
-
-if [[ ! -e "${HOME}/.oh-my-zsh/themes/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
-  ln -s "$(brew --prefix powerlevel10k)/powerlevel10k.zsh-theme" \
-    "${HOME}/.oh-my-zsh/themes/powerlevel10k/powerlevel10k.zsh-theme"
-fi
-
 # Symlink platform-agnostic and platform-specific files to home directory
 create_links "${HOME}/.dotfiles/Default"
 create_links "${HOME}/.dotfiles/$(uname)"
@@ -55,6 +46,15 @@ install_script "${HOME}/.dotfiles/bin/zprofile.sh" "${HOME}/.zprofile"
 
 install_script "${HOME}/.aliases" "${HOME}/.zshrc"
 install_script "${HOME}/.oh-my-zsh/config" "${HOME}/.zshrc"
+
+brew bundle --file="${HOME}/.Brewfile"
+
+mkdir -p "${HOME}/.oh-my-zsh/themes/powerlevel10k"
+
+if [[ ! -e "${HOME}/.oh-my-zsh/themes/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  ln -s "$(brew --prefix powerlevel10k)/powerlevel10k.zsh-theme" \
+    "${HOME}/.oh-my-zsh/themes/powerlevel10k/powerlevel10k.zsh-theme"
+fi
 
 [[ ! -e "${HOME}/.secrets" ]] && touch "${HOME}/.secrets"
 
